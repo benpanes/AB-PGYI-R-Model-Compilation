706a84c058067b56c2308be74cd4a29b94d3a9e0
benpanes <bpanes@ualberta.ca>
2025-02-05 15:21:48 -0700

    Volume calculation overhaul

--- GYPSY/GYPSY subscripts/2 Compile B Trees.R
+++ GYPSY/GYPSY subscripts/2 Compile B Trees.R
@@ -9,208 +9,185 @@ trees <- trees[!(trees$tree_type%in%c("T","ET") & (trees$tree_plot_area==0 | is.
                !(trees$tree_type%in%c("S","ES") & (trees$sapling_plot_area==0 | is.na(trees$sapling_plot_area))) &
                !(trees$tree_type%in%c(paste0("R",1:10)) & (trees$regen_plot_area==0 | is.na(trees$regen_plot_area))),]
 
-trees$index_1 <- paste(trees$company, trees$company_plot_number, trees$measurement_number, trees$tree_number, sep = "_")
+trees[,index_1:=paste(company, company_plot_number, measurement_number, tree_number, sep = "_")]
  
-trees$ba <- ((trees$dbh/200)^2)*3.14159
+trees[,ba:=((dbh/200)^2)*3.14159]
 
-i <- trees$tree_type%in%c("T","ET")
-trees$sph[i] <- 10000/trees$tree_plot_area[i]
 
-i <- trees$tree_type%in%c("S1","ES1")
-trees$sph[i] <- 10000/trees$sapling_plot_area[i]
+trees[tree_type%in%c("T","ET"),sph:= 10000/tree_plot_area]
 
-i <- trees$tree_type%in%c(paste0("R",1:10))
-trees$sph[i] <- 10000/(trees$regen_plot_area[i]*trees$number_regen_plots[i])
+trees[tree_type%in%c("S1","ES1"),sph:= 10000/sapling_plot_area]
 
-i <- trees$tree_type=="B"
-trees$sph[i] <- 0
-trees$ba[i] <- 0
+trees[tree_type%in%c(paste0("R",1:10)),sph:= 10000/(regen_plot_area*number_regen_plots)]
+
+trees[tree_type=="B",c("sph","ba"):=0] # test first
+#trees[tree_type=="B",c("sph","ba"):=list(0,0)]
 
 # assign new species names 
-trees <- trees %>%
-  mutate(species = case_when(
-    species_og %in% c("AW", "AX") ~ "AW",
-    species_og == "BW" ~ "BW",
-    species_og == "PB" ~ "PB",
-    species_og %in% c("LA", "LT", "LW") ~ "LT",
-    species_og %in% c("P", "PX", "PF", "PL", "PW") ~ "PL",
-    species_og == "PJ" ~ "PJ",
-    species_og %in% c("FA", "FB") ~ "FB",
-    species_og == "FD" ~ "FD",
-    species_og %in% c("SE", "SW", "SX") ~ "SW",
-    species_og == "SB" ~ "SB"
-  ))
+trees[,species:=fcase(
+  species_og %in% c("AW","AX"), "AW",
+  species_og %in% c("BW","PB","PJ","FD","SB"), species,
+  species_og %in% c("LA", "LT", "LW"), "LT",
+  species_og %in% c("P", "PX", "PF", "PL", "PW"), "PL",
+  species_og %in% c("FA", "FB"), "FB",
+  species_og %in% c("SE", "SW", "SX"), "SW")]
 
 # changed species to species_og for testing purpose
-trees$natsub[trees$species%in%c("BW","FD","LT","PJ","SE")] <- 0
+trees[species%in%c("BW","FD","LT","PJ","SE"), natsub:=0]
 
-trees.tap <- left_join(trees,fread("GYPSY data/lookup/taper.csv"),by=c("species","natsub"))
-trees.tap <- trees.tap[,.(company,company_plot_number,tree_number,measurement_number,unique,height,dbh,b1,b2,b3,b4,b5,a0,a1,a2,k7,k8)];gc()
+trees[,table(natural_subregion,natsub)]
 
-## Size parameters
-p <- 0.2250
-stumpH <- 0.3
-topdib <- list("T"=0.0125,
-               "M1"=7,
-               "M2"=10)
-stumpD <- list(13,15)
-min_mlen <- 3.66
-
-## Volume formulae
-# Height ratio formula
-acc <- 0.00000001
-r1 <- 1
-r0 <- 0.8
-
-hr <- function(df,i,d,diff=1){
-  hr1 <- function(df,i){
-    df$b1[i]*r0^2+df$b2[i]*log(r0+0.001)+df$b3[i]*sqrt(r0)+df$b4[i]*exp(r0)+df$b5[i]*(df$dbh[i]/df$height[i])
-  }
-  hr2 <- function(df,i,d){
-    (1-((d/(df$a0[i]*df$dbh[i]^df$a1[i]*df$a2[i]^df$dbh[i]))^(1/hr1(df,i)))*(1-sqrt(p)))^2
-  }
-  tryCatch({
-    withTimeout({
-      while(diff>acc){
-        r1 <- hr2(df,i,d)
-        r0 <- (r1+r0)/2
-        diff <- abs(r1-r0)
+# Volume function
+calc_volume <- function(table,spp="species",ht="height",dbh="dbh",natsub="natural_subregion",merch="blank",max.i=1000){
+  hr <- function(df,topdib,max.i=1000){
+    df[,diff:=ifelse(dbh>topdib,1,NA)]
+    df[,r0:=ifelse(dbh>topdib,1,NA)]
+    df[,r1:=ifelse(dbh>topdib,1,NA)]
+    hr1 <- function(b1,b2,b3,b4,b5,dbh,ht,r0){
+      b1*r0^2+b2*log(r0+0.001)+b3*sqrt(r0)+b4*exp(r0)+b5*(dbh/ht)
+    }
+    hr2 <- function(a0,a1,a2,b1,b2,b3,b4,b5,dbh,ht,r0,topdib){
+      (1-((topdib/(a0*dbh^a1*a2^dbh))^(1/hr1(b1,b2,b3,b4,b5,dbh,ht,r0)))*(1-sqrt(0.2250)))^2
+    }
+    n=0
+    while(df[,max(diff,na.rm=T)]>0.00000001){
+      df[dbh>topdib & diff>0.00000001,r1:=hr2(a0,a1,a2,b1,b2,b3,b4,b5,dbh,ht,r0,topdib)]
+      df[dbh>topdib & diff>0.00000001,r0:=(r1+r0)/2]
+      df[dbh>topdib & diff>0.00000001,diff:=abs(r1-r0)]
+      n=n+1
+      if(n>max.i){
+        message("Max height ratio iterations reached.")
+        message(paste("company","company_plot_number","tree_number","measurement_number",natsub,spp,ht,dbh,"diff"))
+        for(i in 1:df[diff>0.00000001,.N]){
+          message(df[diff>0.00000001,paste(company,company_plot_number,tree_number,measurement_number,natsub,spp,ht,dbh,diff)][i])
+        }
+        break
       }
-    },timeout=1)
-  }, TimeoutException=function(ex){
-    warning(paste0("Timeout. Skipping row'",i,"'"))
-  })
-  r0
-}
-
-# Diameter inside bark formula
-dib <- function(df,i,x){
-  (df$a0[i]*df$dbh[i]^df$a1[i])*(df$a2[i]^df$dbh[i])*((1-sqrt(x/df$height[i]))/(1-sqrt(p)))^
-    (df$b1[i]*(x/df$height[i])^2+df$b2[i]*log(x/df$height[i]+0.001)+df$b3[i]*sqrt(x/df$height[i])+df$b4[i]*exp(x/df$height[i])+df$b5[i]*df$dbh[i]/df$height[i])  
-}
-
-# Volume formula
-fvol <- function(df,i){
-  svol <- function(df,i){(2*df$sl[i]/6)*((1/200)^2*pi)*(d0^2+4*d1^2+d2^2)}
-  v <- c()
-  h0 <- stumpH
-  d0 <- df$dibs[i]
-  h1 <- h0 + df$sl[i]
-  d1 <- dib(df,i,h1)
-  h2 <- h1 + df$sl[i]
-  d2 <- dib(df,i,h2)
-  v[1] <- svol(df,i)
-  for(n in 2:10){
-    h0 <- h2
-    d0 <- d2
-    h1 <- h0 + df$sl[i]
-    d1 <- dib(df,i,h1)
-    h2 <- h1 + df$sl[i]
-    d2 <- dib(df,i,h2)
-    v[n] <- svol(df,i)
+    }
   }
-  sum(v)
+  dib <- function(a0,a1,a2,b1,b2,b3,b4,b5,dbh,dheight,ht,r=(dheight/ht)){
+    (a0*dbh^a1)*(a2^dbh)*((1-sqrt(0.2250)))^(b1*(r)^2+b2*log(r+0.001)+b3*sqrt(r)+b4*exp(r)+b5*dbh/ht)
+  }
+  fvol <- function(sl,stumpH,dbh,ht,a0,a1,a2,b1,b2,b3,b4,b5){
+    svol <- function(sl,d0,d1,d2){(2*sl/6)*((1/200)^2*pi)*(d0^2+4*d1^2+d2^2)}
+    v <- data.table()
+    h0 <- stumpH
+    d0 <- dib(a0,a1,a2,b1,b2,b3,b4,b5,dbh,stumpH,ht)
+    h1 <- h0+sl
+    d1 <- dib(a0,a1,a2,b1,b2,b3,b4,b5,dbh,h1,ht)
+    h2 <- h1+sl
+    d2 <- dib(a0,a1,a2,b1,b2,b3,b4,b5,dbh,h2,ht)
+    v[,v1:=svol(sl,d0,d1,d2)]
+    for(n in 2:10){
+      h0 <- h2
+      d0 <- d2
+      h1 <- h0+sl
+      d1 <- dib(a0,a1,a2,b1,b2,b3,b4,b5,dbh,h1,ht)
+      h2 <- h1+sl
+      d2 <- dib(a0,a1,a2,b1,b2,b3,b4,b5,dbh,h2,ht)
+      v[,paste0("v",n):=svol(sl,d0,d1,d2)]
+    }
+    rowSums(v)
+  }
+  if(merch=="blank"){
+    message("Merchantability not specified. Calculating total volume by default")
+    topdib = 0.0125
+    merch="total"
+  } else if(merch=="total"){
+    topdib = 0.0125
+  } else if(merch=="1307"){
+    topdib = 7
+    stumpD = 13
+    minlen = 3.66
+  } else if(merch=="1510"){
+    topdib = 10
+    stumpD = 15
+    minlen = 3.66
+  } else {
+    stop("Invalid merch parameter. Use 'total', '1307', or '1510'")
+  }
+  df <- copy(data.table(table))
+  setnames(df,c(dbh,ht,spp,natsub),c("dbh","ht","spp","natsub"),skip_absent=T)
+  if(any(is.na(dbh) | is.na(ht))){
+    stop("Table contains missing height or DBH.")
+  }
+  stumpH = 0.3
+  df[,spp:=toupper(spp)]
+  if(any(!df[,spp]%in%c("AW","AX","BW","PB",
+                        "LA","LT","LW",
+                        "P","PX","PF","PL","PW","PJ",
+                        "FA","FB","FD",
+                        "SE","SW","SX",
+                        "SB"))){
+    stop("Invalid species. Accepted codes are: AW, AX, BW, PB, LA, LT, LW, P, PX, PF, PL, PW, PJ, FA, FB, FD, SE, SW, SX, or SB")
+  }
+  if(any(!df[,natsub]%in%c("CM","DM","NM","BSA","PAD","UBH","LBH","ALP","SA","MT","UF","LF","AP","KU","FP","PRP","CP","DMG","FF","NF","MG"))){
+    stop("Invalid natural subregion. Accepted codes are: CM, DM, NM, BSA, PAD, UBH, LBH, ALP, SA, MT, UF, LF, AP, KU, FP, PRP, CP, DMG, FF, NF, MG")
+  }
+  df[,spp:=fcase(spp%in%c("AW","AX"),"AW",
+                 spp%in%c("LA","LT","LW"),"LT",
+                 spp%in%c("P","PX","PF","PL","PW"),"PL",
+                 spp%in%c("FA","FB"),"FB",
+                 spp%in%c("SE","SW","SX"),"SW",
+                 spp%in%c("BW","PB","PJ","FD","SB"),spp)]
+  df[,c(                                                                                                                   "a0",     "a1",     "a2",     "b1",      "b2",      "b3",      "b4",      "b5",      "k7",      "k8"):=
+       rbindlist(fcase(
+         spp=="AW" &           natsub%in%c(NA,"DMG","FF","NF","MG"),.(.(                                                   0.790406, 1.026943, 0.997524, 0.600584,  -0.065681, -0.173812, 0.121363,  0.063253,  0.091433,  1.077082)),
+         spp=="AW" &           natsub%in%c("CM","NM","BSA","PAD","UBH","LBH","AP","KU"),.(.(                               0.841897, 0.997064, 0.998713, 0.536865,  -0.06402,  -0.234471, 0.179963,  0.03155,   0.061755,  1.079512)),
+         spp=="AW" &           natsub%in%c("DM","FP","PRP","CP"),.(.(                                                      0.944522, 0.93803,  1.001644, 0.695363,  -0.067849, 0.050603,  -0.01633,  0.116432,  0.024127,  1.081189)),
+         spp=="AW" &           natsub%in%c("ALP","SA","UF"),.(.(                                                           0.588838, 1.161895, 0.992096, 0.7093,    -0.075446, -0.116041, 0.040949,  0.113638,  0.21134,   1.073573)),
+         spp=="AW" &           natsub%in%c("MT","LF"),.(.(                                                                 0.905615, 0.964894, 1.000054, 0.553236,  -0.049737, -0.280768, 0.170687,  0.075789,  0.135261,  1.072734)),
+         spp=="BW",.(.(                                                                                                    0.894358, 1.007721, 0.991384, -0.483072, 0.155593,  -2.273122, 1.326501,  0.168897,  0.077197,  1.062798)),
+         spp%in%c("FA","FB") & natsub%in%c(NA,"DMG","FF","NF","MG"),.(.(                                                   1.002016, 0.944076, 0.999921, 1.33633,   -0.320352, 2.839497,  -1.324815, 0.077452,  0.28994,   1.051003)),
+         spp%in%c("FA","FB") & natsub%in%c("CM","DM","NM","BSA","PAD","UBH","LBH","MT","LF","AP","KU","FP","PRP","CP"),.(.(0.918647, 0.990225, 0.997292, 1.568514,  -0.384262, 3.503466,  -1.677185, 0.128169,  0.249035,  1.050236)),
+         spp%in%c("FA","FB") & natsub%in%c("ALP","SA","UF"),.(.(                                                           1.108006, 0.89838,  1.001816, 1.338336,  -0.30463,  2.694363,  -1.277617, 0.087438,  0.323616,  1.050716)),
+         spp=="FD",.(.(                                                                                                    0.913153, 0.964386, 0.998391, 1.386315,  -0.286495, 1.783899,  -0.916932, 0.05883,   -0.095253, 1.123153)),
+         spp=="LT",.(.(                                                                                                    0.933517, 0.965471, 0.998393, 2.079455,  -0.462028, 3.732057,  -1.950194, 0.190425,  0.37887,   1.034008)),
+         spp=="PB" &           natsub%in%c(NA,"DMG","FF","NF","MG"),.(.(                                                   0.861179, 0.951483, 1.000957, 0.752581,  -0.167305, 0.693611,  -0.224137, 0.008214,  0.149322,  1.117988)),
+         spp=="PB" &           natsub%in%c("CM","DM","NM","BSA","PAD","UBH","LBH","AP","KU","PRP","CP"),.(.(               0.80437,  0.982874, 0.999527, 0.996958,  -0.223248, 1.106731,  -0.459817, -0.003392, 0.109731,  1.12512)),
+         spp=="PB" &           natsub%in%c("ALP","SA","MT","UF","LF","FP"),.(.(                                            0.913329, 0.92259,  1.002574, 0.308448,  -0.06567,  -0.10213,  0.226336,  0.023148,  0.257085,  1.10315)),
+         spp=="PJ",.(.(                                                                                                    0.940832, 0.955575, 0.999333, 0.116311,  -0.028172, -0.384427, 0.304055,  0.072192,  0.161727,  1.045672)),
+         spp=="PL" &           natsub%in%c(NA,"DMG","FF","NF","MG"),.(.(                                                   0.897617, 0.988518, 0.998735, 0.675759,  -0.130313, 0.570634,  -0.275457, 0.105403,  0.283173,  1.025305)),
+         spp=="PL" &           natsub%in%c("CM","DM","NM","PAD","AP","KU","PRP","CP"),.(.(                                 1.033572, 0.913621, 1.000765, 0.256633,  -0.049091, -0.252118, 0.174267,  0.123722,  0.189744,  1.04681)),
+         spp=="PL" &           natsub%in%c("BSA","UF"),.(.(                                                                0.828665, 1.024196, 0.997492, 0.596193,  -0.118777, 0.465591,  -0.196176, 0.083094,  0.308258,  1.024549)),
+         spp=="PL" &           natsub%in%c("UBH","LBH","MT","LF","FP"),.(.(                                                0.957164, 0.959992, 0.999774, 0.766747,  -0.140758, 0.666037,  -0.35505,  0.13214,   0.294015,  1.024582)),
+         spp=="PL" &           natsub%in%c("ALP","SA"),.(.(                                                                0.800648, 1.053544, 0.995568, 0.568347,  -0.125114, 0.610085,  -0.238442, 0.045398,  0.240347,  1.020105)),
+         spp=="SB" &           natsub%in%c(NA,"DMG","FF","MF","MG"),.(.(                                                   0.940695, 0.957211, 0.99964,  1.395784,  -0.344672, 2.835917,  -1.39646,  0.152487,  0.382746,  1.033405)),
+         spp=="SB" &           natsub%in%c("CM","DM","NM","BSA","PAD","UBH","LBH","AP","KU","FP","PRP","CP"),.(.(          0.929037, 0.967718, 0.998511, 1.236597,  -0.308204, 2.535507,  -1.22206,  0.146243,  0.349765,  1.036689)),
+         spp=="SB" &           natsub%in%c("ALP","SA","MT","UF","LF"),.(.(                                                 0.957624, 0.94674,  1.000452, 1.430462,  -0.356702, 2.950725,  -1.455471, 0.154263,  0.414614,  1.030781)),
+         spp=="SE",.(.(                                                                                                    1.072576, 0.897766, 1.001919, 1.301834,  -0.305439, 2.265717,  -1.119671, 0.123519,  0.46127,   1.023752)),
+         spp=="SW" &           natsub%in%c(NA,"DMG","FF","NF","MG"),.(.(                                                   0.860438, 0.995406, 0.998493, 1.040218,  -0.252387, 1.842818,  -0.852227, 0.110359,  0.484768,  1.024893)),
+         spp=="SW" &           natsub%in%c("CM","DM","NM","BSA","PAD","UBH","LBH","AP","KU","PRP","CP"),.(.(               0.903528, 0.975136, 0.999018, 0.846981,  -0.244969, 1.783097,  -0.730236, 0.040997,  0.413577,  1.028342)),
+         spp=="SW" &           natsub%in%c("ALP","SA","UF"),.(.(                                                           0.713393, 1.071533, 0.996067, 1.153679,  -0.283807, 2.022713,  -0.953783, 0.101608,  0.521645,  1.024172)),
+         spp=="SW" &           natsub%in%c("MT","LF","FP"),.(.(                                                            0.862685, 0.993148, 0.998773, 1.135018,  -0.252377, 1.885321,  -0.921437, 0.150228,  0.536767,  1.0227))))]
+  hr(df,topdib)
+  df[,dibs:=dib(a0,a1,a2,b1,b2,b3,b4,b5,dbh,stumpH,ht)]
+  df[,dobs:=k7+k8*dibs]
+  if(merch=="total"){
+    df[,mh:=ht*r0]
+    df[,ml:=mh-stumpH]
+    df[,sl:=ml/20]
+    df[,vol:=fvol(sl,stumpH,dbh,ht,a0,a1,a2,b1,b2,b3,b4,b5)]
+    df[,tvol:=vol+pi*(topdib/200)^2*(ht-mh)/3+pi*(dibs/200)^2*stumpH]
+    df[is.na(tvol),tvol:=0]
+    return(df[,tvol ])
+  } else {
+    df[dobs>stumpD,mh:=ht*r0]
+    df[dobs>stumpD,ml:=mh-stumpH]
+    df[ml>minlen,sl:=ml/20]
+    df[ml>minlen,vol:=fvol(sl,stumpH,dbh,ht,a0,a1,a2,b1,b2,b3,b4,b5)]
+    df[is.na(vol),vol:=0]
+    return(df[,vol])
+  }
 }
 
-#### Calculate merchantable volume 13/7
-temp <- trees.tap[dbh>topdib[["M1"]],]
-
-## Merchantable height ratio
-len <- nrow(temp)
-cl <- makeCluster(cluster.num)
-clusterEvalQ(cl,library(R.utils))
-clusterExport(cl,c("hr","acc","topdib","temp","r0","r1","p"))
-temp$r0 <- unlist(parLapply(cl,1:len,function(i) hr(temp,i,topdib[["M1"]])))
-stopCluster(cl)
-
-## Stump diameters
-temp$dibs <- dib(temp,1:nrow(temp),stumpH)
-temp$dobs <- temp$k7+temp$k8*temp$dibs
-
-## Merchantable volume
-temp <- temp[temp$dobs>=stumpD[[1]],]
-temp$mh <- temp$height*temp$r0
-temp$ml <- temp$mh-stumpH
-temp$sl <- temp$ml/20
-temp <- temp[ml>min_mlen,]
-
-len <- nrow(temp)
-cl <- makeCluster(cluster.num)
-clusterExport(cl,c("temp","p","stumpH","dib","fvol"))
-temp$vol_1307 <- unlist(parLapply(cl,1:len,function(x) fvol(temp,x)))
-stopCluster(cl)
-
-trees.vol <- left_join(trees,temp[,.(company,company_plot_number,tree_number,measurement_number,vol_1307)],by=c("company","company_plot_number","tree_number","measurement_number"))
-trees.vol[is.na(vol_1307),"vol_1307"] <- 0
-rm(temp);gc()
-
-#### Calculate merchantable volume 15/10
-temp <- trees.tap[dbh>topdib[["M2"]]]
-
-## Merchantable height ratio
-len <- nrow(temp)
-cl <- makeCluster(cluster.num)
-clusterEvalQ(cl,library(R.utils))
-clusterExport(cl,c("hr","acc","topdib","temp","r0","r1","p"))
-temp$r0 <- unlist(parLapply(cl,1:len,function(i) hr(temp,i,topdib[["M2"]])))
-stopCluster(cl)
-
-## Stump diameters
-temp$dibs <- dib(temp,1:nrow(temp),stumpH)
-temp$dobs <- temp$k7+temp$k8*temp$dibs
-
-## Merchantable volume
-temp <- temp[temp$dobs>=stumpD[[2]],]
-temp$mh <- temp$height*temp$r0
-temp$ml <- temp$mh-stumpH
-temp$sl <- temp$ml/20
-temp <- temp[ml>min_mlen,]
-
-len <- nrow(temp)
-cl <- makeCluster(cluster.num)
-clusterExport(cl,c("temp","p","stumpH","dib","fvol"))
-temp$vol_1510 <- unlist(parLapply(cl,1:len,function(x) fvol(temp,x)))
-stopCluster(cl)
-
-trees.vol <- left_join(trees.vol,temp[,.(company,company_plot_number,tree_number,measurement_number,vol_1510)],by=c("company","company_plot_number","tree_number","measurement_number"))
-trees.vol[is.na(vol_1510),"vol_1510"] <- 0
-rm(temp);gc()
-
-#### Calculate total volume
-temp <- trees.tap[dbh>topdib[["T"]],]
-
-## Height ratio
-len <- nrow(temp)
-cl <- makeCluster(cluster.num)
-clusterEvalQ(cl,library(R.utils))
-clusterExport(cl,c("hr","acc","topdib","temp","r0","r1","p"))
-temp$r0 <- unlist(parLapply(cl,1:len,function(i) hr(temp,i,topdib[["T"]])))
-stopCluster(cl)
-
-## Stump diameters
-temp$dibs <- dib(temp,1:nrow(temp),stumpH)
-temp$dobs <- temp$k7+temp$k8*temp$dibs
-
-## Total volume
-temp$mh <- temp$height*temp$r0
-temp$ml <- temp$mh-stumpH
-temp$sl <- temp$ml/20
-
-len <- nrow(temp)
-cl <- makeCluster(cluster.num)
-clusterExport(cl,c("temp","p","stumpH","dib","fvol"))
-tvol <- unlist(parLapply(cl,1:len,function(x) fvol(temp,x)))
-stopCluster(cl)
-temp$vol_0000 <- tvol+pi*(topdib[["T"]]/200)^2*(temp$height-temp$mh)/3+pi*(temp$dibs/200)^2*stumpH
-
-trees.vol <- left_join(trees.vol,temp[,.(company,company_plot_number,tree_number,measurement_number,vol_0000)],by=c("company","company_plot_number","tree_number","measurement_number"))
-trees.vol[is.na(vol_0000),"vol_0000"] <- 0
-
-##### Left off at the at the end of the volume calculation section (before biomass)
+trees[,vol_0000:=calc_volume(trees,merch="total")]
+trees[,vol_1307:=calc_volume(trees,merch="1307")]
+trees[,vol_1510:=calc_volume(trees,merch="1510")]
 
 # biomass and carbon calculation
 
-trees.bio <- trees.vol %>%
+trees.bio <- trees %>%
   mutate(
     biomass = if_else(dbh < 15 | vol_1307 == 0,
                       case_when(
@@ -267,6 +244,6 @@ tree_ages <- trees.bio %>%
 
 fwrite(tree_ages,"GYPSY data/intermediate/i_tree_age.csv")
 
-minutes <- round(Sys.time()-start_time)
+minutes <- floor(Sys.time()-start_time)
 seconds <- round((Sys.time()-start_time-minutes)*60)
 message(paste(minutes,"minutes and",seconds,"seconds for compile trees"))
